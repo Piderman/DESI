cb5e4355b1d6ee472a15249fa2f7e9a4afd6071a
Matty <matty@mattycollins.com.au>
2014-06-01 22:49:03 +1000

    work on gulp and documentation

---
+++ devops/backend.md
@@ -0,0 +1,8 @@
+---
+layout: default
+title: backend
+---
+
+## `gulp`
+Default watch task to complile development CSS. Livereload, although tempremental, for added awesomeness
+
--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,63 @@
 // gulp and packages setup
 var gulp = require('gulp'),
 	sass = require('gulp-ruby-sass'),
-	prefix = require('gulp-autoprefixer');
+	prefix = require('gulp-autoprefixer'),
+	livereload = require('gulp-livereload'),
+	shell = require('gulp-shell');
 	// concat = require('gulp-concat'),
 	// uglify = require('gulp-uglify');
 
+// project settings
+project = {
+	sass__dir : '_standard/styles/*.scss',
+	build__dir : 'build'
+};
 
 gulp.task('sass', function() {
 
 	// give it options for dev or production?
-	return gulp.src('_standard/styles/*.scss')
+	return gulp.src(project.sass__dir)
 		.pipe(sass({
 			sourcemap: true
 		}))
-		.pipe(gulp.dest('build'));
+		.pipe(gulp.dest(project.build__dir))
+		.pipe(livereload());
 });
+
 
 // Watch Files For Changes
 gulp.task('watch', function() {
+
 	// gulp.watch('js/*.js', ['lint', 'scripts']);
 	gulp.watch('_standard/styles/*.scss', ['sass']);
+
 });
+
+
+
+
+//----------------------------------------------------------------------------
+
+
+// production settings, readying for deployment
+
+gulp.task('sass--prod', function() {
+	return gulp.src(project.sass__dir)
+		.pipe(sass({
+			style : 'compressed'
+		}))
+		.pipe(gulp.dest(project.build__dir));
+});
+
+gulp.task('housekeeping', shell.task([
+	// remove maps (if exist)
+	'rm -f build/*.map',
+
+	// build jekyll so we can have look. warn: shouldn't be running already
+	'jekyll serve'
+]));
+
 
 // Default Task
 gulp.task('default', ['watch']);
+gulp.task('production', ['sass--prod', 'housekeeping']);
